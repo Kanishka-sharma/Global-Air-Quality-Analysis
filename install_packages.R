b57410f62beef425fb0c2437aa90fb810ca89805
Kanishka Sharma <Kanishka13@ymail.com>
2025-10-28 18:06:06 +0000

    Adding Files

---
+++ install_packages.R
@@ -0,0 +1,16 @@
+# Run this script to install all the required packages
+
+install.packages(c(
+  "dplyr",
+  "corrplot",
+  "VIM",
+  "ggplot2",
+  "gridExtra",
+   "mice",
+   "FactoMineR",
+   "factoextra",
+   "cluster",
+   "mclust",
+   "naniar",
+   "abind" 
+))
